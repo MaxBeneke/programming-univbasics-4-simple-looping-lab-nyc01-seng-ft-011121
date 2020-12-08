0ac6d8cba05d5318b83b525b6fda8b610bf170c1
Max Beneke <max.beneke@gmail.com>
2020-12-08 15:23:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,12 +22,3 @@ def output_array(array)
   end 
 end
 
-def return_string_array(array)
-  count = 0 
-  while count < array.length 
-  do 
-    new_array.push(array[count].to_s)
-    count += 1 
-  end 
-  new_array
-end
